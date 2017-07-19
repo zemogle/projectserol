3c8a46ffda9398f3264a672b1e0188a45a527e14
Edward Gomez <edward@gomez.me.uk>
2017-07-19 16:49:42 +0100

    Initial commit

---
+++ api.py
@@ -0,0 +1,54 @@
+'''
+Project SEROL
+
+This is a set of test API endpoints
+'''
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class BaseApi(Resource):
+    def get(self):
+        return {'status': 'ok'}
+
+
+class ChallengeApi(Resource):
+    def get(self):
+        status =  {'mission':1,'id':3,'challenge':'Observe a galaxy','status':'done'}
+        return status
+
+class MissionApi(Resource):
+    def get(self):
+        status = {'id':1,'mission':'Beginner Level','progress':0.5}
+        return status
+
+class StickersApi(Resource):
+    def get(self):
+        status = {'stickers': [{'challenge':1, 'name':'First Observation', 'file':''},
+                                {'challenge':2, 'name':'Observe a star cluster', 'file':''},
+                                {'challenge':3, 'name':'Observe a galaxy', 'file':''}
+                                ]}
+        return status
+
+class SerolApi(Resource):
+    def get(self):
+        status = {'status': 'ok'}
+        return status
+
+class RequestApi(Resource):
+    def post(self):
+        status = {'status': 'ok'}
+        return status
+
+
+api.add_resource(BaseApi, '/')
+api.add_resource(MissionApi, '/mission/')
+api.add_resource(StickersApi, '/stickers/')
+api.add_resource(SerolApi, '/serol/')
+api.add_resource(RequestApi, '/request/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+from sense_hat import SenseHat
+
+URL_BASE = 'http://10.73.224.41:5000'
+URL_MISSION = '{}/mission/'
+URL_CHALLENGE = '{}/challenge/'
+URL_STATUS = '{}/status/'
+
+def check_status():
+    resp = requests.get(URL_STATUS).json()
+    if resp['status'] == 'ok':
+        send_message('OK', colour=(0,255,0))
+
+def send_message(msg,colour):
+    sense = SenseHat()
+    sense.set_rotation(180)
+    sense.show_message(msg, text_colour=colour)
+
+if __name__ == '__main__':
+    check_status()
