1cb0a7184872e2654f255329dd017aa07511b0ef
Edward Gomez <edward@gomez.me.uk>
2017-07-20 14:16:22 +0100

    Adding images

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from sense_hat import SenseHat
 URL_BASE = 'http://10.73.224.41:5000'
 URL_MISSION = '{}/mission/'.format(URL_BASE)
 URL_CHALLENGE = '{}/challenge/'.format(URL_BASE)
-URL_STATUS = '{}/status/'.format(URL_BASE)
+URL_STATUS = '{}/'.format(URL_BASE)
 
 def check_status():
     resp = requests.get(URL_STATUS).json()
     if resp['status'] == 'ok':
-        send_message('OK', colour=(0,255,0))
+        send_message("tick.png", colour=(0,255,0))
 
-def send_message(msg,colour):
+def send_message(isg,colour):
     sense = SenseHat()
     sense.set_rotation(180)
-    sense.show_message(msg, text_colour=colour)
+    sense.load_image(img)
 
 if __name__ == '__main__':
     check_status()
